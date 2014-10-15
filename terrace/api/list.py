a5e63e5edeeaa6c2912d243c2f95c4cd51d05117
Raghav sidhanti <sudohippie@users.noreply.github.com>
2014-10-15 11:31:12 -0700

    Revert "#5: integrated list rest with list dao"
    This reverts commit 5d0bcb69da1f6d69b83cc3d60527d8ef30854fec.

--- terrace/api/list.py
+++ terrace/api/list.py
@@ -30,7 +30,6 @@ def all_lists():
 
 @list_api.route('/list/<int:list_id>', methods=['GET', 'PUT', 'DELETE'])
 def list_by_id(list_id):
-    res = None
     if request.method == 'GET':
         one_list = list_dao.get_list_by_id(list_id)
         res = jsonify(get_list_as_dict(one_list))
@@ -38,31 +37,27 @@ def list_by_id(list_id):
         if one_list is None:
             res.status_code = NOT_FOUND
 
+        return res
+
     elif request.method == 'PUT':
         name = request.json.get('name')
         if name is None:
-            res = jsonify({'error': 'Name is required'})
-            res.status_code = BAD_REQUEST
+            resp = jsonify({'error': 'Name is required'})
+            resp.status_code = BAD_REQUEST
+            return resp
         else:
             l = list_dao.update_list(list_id=list_id, name=name)
             res = jsonify(get_list_as_dict(l))
             if l is None:
                 res.status_code = NOT_FOUND
+            return res
 
     elif request.method == 'DELETE':
         l = list_dao.delete_list(list_id)
         res = jsonify(get_list_as_dict(l))
         if l is None:
             res.status_code = NOT_FOUND
-
-    return res
-
-
-@list_api.errorhandler(NOT_FOUND)
-def not_found(error):
-    res = jsonify({'error': 'resource not found'})
-    res.status_code = NOT_FOUND
-    return res
+        return res
 
 
 def get_list_as_dict(l):
