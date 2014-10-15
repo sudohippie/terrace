5f4dffc24280cac6050e77d78c3ab65318330312
Raghav sidhanti <sidhantirc@gmail.com>
2014-10-14 20:28:35 -0700

    #2: Rest api handers for item by id

--- terrace/api/item.py
+++ terrace/api/item.py
@@ -13,3 +13,14 @@ def all_items():
     elif request.method == 'POST':
         return jsonify({'id': 'items', 'method': 'POST'})
 
+
+@item_api.route('/item/<item_id>', methods=['GET', 'PUT', 'DELETE'])
+def item_by_id(item_id):
+    if request.method == 'GET':
+        return jsonify({'id': item_id, 'method': 'GET'})
+
+    elif request.method == 'PUT':
+        return jsonify({'id': item_id, 'method': 'PUT'})
+
+    elif request.method == 'DELETE':
+        return jsonify({'id': item_id, 'method': 'DELETE'})
