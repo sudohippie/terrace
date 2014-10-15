480d905b87959278f28a48dd06e4048910ff7214
Raghav sidhanti <sidhantirc@gmail.com>
2014-10-14 21:57:24 -0700

    fixed #3: Rest api handles for list item

--- terrace/api/list_item.py
+++ terrace/api/list_item.py
@@ -1,1 +1,26 @@
 __author__ = 'rsidhanti'
+
+from flask import Blueprint
+from flask import request, jsonify
+
+list_item_api = Blueprint('list_item_api', __name__)
+
+
+@list_item_api.route('/list/<list_id>/items', methods=['GET', 'POST'])
+def list_items(list_id):
+    if request.method == 'GET':
+        return jsonify({'id': list_id, 'method': 'GET'})
+    elif request.method == 'POST':
+        return jsonify({'id': list_id, 'method': 'POST'})
+
+
+@list_item_api.route('/list/<list_id>/item/<item_id>', methods=['GET', 'PUT', 'DELETE'])
+def list_item_by_id(list_id, item_id):
+    if request.method == 'GET':
+        return jsonify({'list_id': list_id, 'item_id': item_id, 'method': 'GET'})
+
+    elif request.method == 'PUT':
+        return jsonify({'list_id': list_id, 'item_id': item_id, 'method': 'PUT'})
+
+    elif request.method == 'DELETE':
+        return jsonify({'list_id': list_id, 'item_id': item_id, 'method': 'DELETE'})
